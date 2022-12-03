1922815563bc23c3adfbd0d8d1b65b3fefcc2063
japheth-dew <solaadewumi2002@gmail.com>
2022-12-03 12:56:43 +0100

    home page done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="dark:bg-black dark text-white bg-white  md:py-[4em] md:px-[6em] p-[2em] font-monts flex md:flex-row flex-col justify-between ">
-      <div>
-        {" "}
-        <div className=" w-[40%] ">
+    <div className="dark:bg-black dark text-white bg-white  md:py-[4em] md:px-[6em] p-[2em] font-monts flex md:flex-row flex-col justify-between  ">
+      <div className="flex-1">
+        <div className="md:w-[60%] w-full">
           <h1 className="font-default md:text-[40px] text-[32px] font-[500] leading-[2em] ">
             ABOUT FORTMEADE
           </h1>
-          <p className="leading-[1.8em] md:text-[18px] ">
-            <p>
+          <p className="leading-[1.8em]  ">
+            <span>
               CREATING QUALITY URBAN LIFESTYLES, BUILDING STRONGER COMMUNITIES
-            </p>
+            </span>{" "}
+            <br />
             Improving quality of life with an integrated unified approach. Our
             team will handle all aspect including bid/award of contracts,
             management of contracts, RFI and submittal trafficking, payment
@@ -20,19 +20,29 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="flex-1">
+
+      <div className="">
         <h1 className="font-default md:text-[40px] text-[32px] font-[500] leading-[2em] ">
-          ABOUT FORTMEADE
+          CONTACT
         </h1>
-        <p className="leading-[1.8em] md:text-[18px] ">
-          <p>
-            CREATING QUALITY URBAN LIFESTYLES, BUILDING STRONGER COMMUNITIES
+        <div>
+          <p className="text-secondary font-default text-[18px] leading-[2em]">
+            PHONE & WHATSAPP
           </p>
-          Improving quality of life with an integrated unified approach. Our
-          team will handle all aspect including bid/award of contracts,
-          management of contracts, RFI and submittal trafficking, payment
-          applications, and close out.
-        </p>
+          <p>(+234) -demo-number</p>
+        </div>
+        <div>
+          <p className="text-secondary font-default text-[18px] leading-[2em]">
+            EMAIL
+          </p>
+          <p>demo-email</p>
+        </div>
+        <div>
+          <p className="text-secondary font-default text-[18px] leading-[2em]">
+            ADDRESS
+          </p>
+          <p>demo address</p>
+        </div>
       </div>
     </div>
   );
